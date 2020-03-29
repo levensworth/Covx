e95737af1c73fa24d6c705af1d518117874ba509
Santiago Bassani <santiago.bassani96@gmail.com>
2020-03-29 18:50:02 -0300

    [FIX] file reading

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ import cv2
 import os
 import json
 
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
@@ -13,16 +15,20 @@ def heart_beat():
 @app.route('/predict', methods=['POST'])
 def predict():
     model = infer.get_model('models/covid19.model')
-    file_data = request.files['file']
-    file_data.save('app/file')
+    # read image directly from post request
+    # https://stackoverflow.com/questions/58082051/how-to-convert-image-file-object-to-numpy-array-in-with-opencv-python
+    file_data = request.files['file'].read()
+    data = cv2.imdecode(np.fromstring(file_data, np.uint8), cv2.IMREAD_COLOR)
 
-    data = cv2.imread('app/file')
     norm_data = infer.normalize_image(data)
+    # result is [postive_prob , negative_prob]
     result = model.predict(norm_data)[0]
+    # create result
     result = {
         "positive": result[0],
         "negative": result[1]
     }
     return  result.__str__()
+
 if __name__ == '__main__':
     app.run()
